363943324122d9fabc8d070ea0a167d42a92160c
Nefedov Danil <eanmos@ya.ru>
2022-05-28 07:36:10 +0200

    tooltips

--- src/viewast.js
+++ src/viewast.js
@@ -108,6 +108,59 @@ class ViewAST {
         self.appendChild(icon);
 
         self.appendChild(label);
+
+        let icon2 = createElem("img", "info-icon");
+        icon2.src = "/img/question.svg";
+        icon2.addEventListener("mouseover", function () {
+            this.classList.toggle("hovered");
+        });
+        icon2.addEventListener("mouseout", function () {
+            this.classList.toggle("hovered");
+        });
+        icon2.addEventListener("click", function () {
+            this.classList.toggle("hovered");
+        });
+
+        let tooltipe = createElem("div", "ast-node-tooltip");
+        tooltipe.innerHTML =
+`<div class='head'>
+    <div class='kind'>
+        <img src='${icon.src}' />
+        <span>${n.kind}</span>
+    </div>
+    ${(n.name) ? `<div class='ast-node-attribute name'>Name: <span class='identifier'>${n.name}</span></div>` : ""}
+    ${(n.c_type) ? `<div class='ast-node-attribute type'>Type: <span class='ctype2'>${n.c_type}</span></div>` : ""}
+    ${(n.storage_class) ? `<div class='ast-node-attribute storage-class-specifier'>Storage Class Specifier: <span class='keyword'>${n.storage_class}</span></div>` : ""}
+</div>`;
+/* <div class='body'>
+        <div>
+            <b><code>5.1.1.1(1)</code></b> A C program need not all be translated at the same
+            time. The text of the program is kept in units called source files,
+            (or preprocessing files) in this International Standard. A source
+            file together with all the headers and source files included via the
+            preprocessing directive include is known as a preprocessing
+            translation unit.  After preprocessing, a preprocessing translation
+            unit is called a translation unit. Previously translated translation
+            units may be preserved individually or in libraries.
+
+            The separate translation units of a program communicate by (for
+            example) calls to functions whose identifiers have external linkage,
+            manipulation of objects whose identifiers have external linkage, or
+            manipulation of data files. 
+        </div>
+</div>`; */
+
+        tippy(icon2, {content: tooltipe, allowHTML: true, theme: 'custom', interactive: true, arrow: null});
+
+        self.appendChild(icon2);
+
+        self.addEventListener("mouseover", function () {
+            icon2.style.display = "inline";
+        });
+        self.addEventListener("mouseout", function () {
+            icon2.style.display = "none";
+        });
+
         node.appendChild(self);
 
         if (n.children && n.children.length > 0) {
